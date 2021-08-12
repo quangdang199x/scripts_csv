969194dae5693f764c1c6adeaf7b056c8b99f595
Quang <phamdangquangbk@gmail.com>
2021-08-12 15:42:33 +0700

    Add new update

--- create_csv.py
+++ create_csv.py
@@ -1,165 +1,184 @@
-from os import error
 from numpy import dtype, flatiter
 import pandas as pd
 from pandas.core.frame import DataFrame
 
 
-df_1 = pd.read_csv("new_info.csv", header=0)
-activePower_numpy = DataFrame(df_1, columns=["active_power"]).values
+active_power = pd.read_csv("active_power.csv", header=0)
+activePower_numpy = DataFrame(active_power, columns=["active_power"]).values
+time = pd.read_csv("time.csv", header=0)
+class CreateCSVfile:
+    def MyInput(
+            asset = None, 
+            scope = None, 
+            thamchieu = None, 
+            web_dailyEnergy = None
+            ):
 
-df_2 = pd.read_csv("inverter1.csv", header=0)
+        x = 0
+        activePower = []
+        for i in activePower_numpy:
+            i = float(i)
+            activePower.append(i)
+            x += 1
 
-x = 0
-activePower = []
-for i in activePower_numpy:
-    i = float(i)
-    activePower.append(i)
-    x += 1
+        y = 0
+        dailyEnergy = []
+        for value in activePower:
+            if value == 0:
+                value = value*0.25 + 0
+                dailyEnergy.append(value)
+            elif value != 0:
+                value = value*0.25 + dailyEnergy[y]
+                dailyEnergy.append(value)
+                y += 1
 
-y = 0
-dailyEnergy = []
-for value in activePower:
-    if value == 0:
-        value = value*0.25 + 0
-        dailyEnergy.append(value)
-    elif value != 0:
-        value = value*0.25 + dailyEnergy[y]
-        dailyEnergy.append(value)
-        y += 1
+        round_dailyEnergy = []
+        for eg  in dailyEnergy:
+            eg = int(round(eg, -3))
+            round_dailyEnergy.append(eg)
 
-round_dailyEnergy = []
-for eg  in dailyEnergy:
-    eg = int(round(eg, -3))
-    round_dailyEnergy.append(eg)
+        k = 1
+        q = 0
+        sub_daliyEnergy = []
+        for giatri in round_dailyEnergy:
+            if giatri == 0:
+                giatri = giatri
+                sub_daliyEnergy.append(giatri)
+            elif giatri != 0:
+                giatri = round_dailyEnergy[k] - round_dailyEnergy[q]
+                sub_daliyEnergy.append(giatri)
+                k += 1
+                q += 1
 
-k = 1
-q = 0
-sub_daliyEnergy = []
-for giatri in round_dailyEnergy:
-    if giatri == 0:
-        giatri = giatri
-        sub_daliyEnergy.append(giatri)
-    elif giatri != 0:
-        giatri = round_dailyEnergy[k] - round_dailyEnergy[q]
-        sub_daliyEnergy.append(giatri)
-        k += 1
-        q += 1
+        energy_15p = []
+        active_energy = [thamchieu]
 
-thamchieu = 90000000 #Example, dung function or input() 
-web_dailyEnergy = 430000 #int(input("Nhap vao so dailyEnergy tren Website: "))
-energy_15p = []
+        if max(round_dailyEnergy) <= web_dailyEnergy:
+            socanbu = web_dailyEnergy - max(round_dailyEnergy)
+            for add in sub_daliyEnergy:
+                if 0 < socanbu <= 5000:
+                    if add == max(sub_daliyEnergy):
+                        if sub_daliyEnergy.count(max(sub_daliyEnergy)) == 1:
+                            add = max(sub_daliyEnergy) + socanbu
+                            phandu = 0
+                        elif sub_daliyEnergy.count(max(sub_daliyEnergy)) == 2:
+                            if socanbu % 2000 == 0:
+                                add = max(sub_daliyEnergy) + socanbu
+                            elif socanbu % 2000 != 0:
+                                add = max(sub_daliyEnergy)  + (socanbu//2000)*1000
+                                phandu = socanbu - 2000*(socanbu//2000)
+                        elif sub_daliyEnergy.count(max(sub_daliyEnergy)) == 3:
+                            if socanbu % 3000 == 0:
+                                add = max(sub_daliyEnergy) + socanbu
+                            elif socanbu % 3000 != 0:
+                                add = max(sub_daliyEnergy) + (socanbu//3000)*1000
+                                phandu = socanbu - 3000*(socanbu//3000)   
+                        else:
+                            print(f"Out range for max peak!. Max peak is: {sub_daliyEnergy.count(max(sub_daliyEnergy))}")
+                        energy_15p.append(add)
+                        
+                    elif add != max(sub_daliyEnergy):
+                        add = add
+                        energy_15p.append(add)             
+                    
+                elif socanbu > 5000:
+                    if add == max(sub_daliyEnergy):
+                        if sub_daliyEnergy.count(max(sub_daliyEnergy)) == 1:
+                            add = max(sub_daliyEnergy) + (socanbu - (socanbu//5000)*5000)
+                            phandu = (socanbu//5000)*5000
+                        elif sub_daliyEnergy.count(max(sub_daliyEnergy)) == 2:
+                            add = max(sub_daliyEnergy) + (socanbu - (socanbu//5000)*5000)
+                            phandu = 2*(socanbu//5000)*5000 - socanbu
+                        elif sub_daliyEnergy.count(max(sub_daliyEnergy)) == 3:
+                            add = max(sub_daliyEnergy) + (socanbu - (socanbu//5000)*5000)
+                            phandu = 3*(socanbu//5000)*5000 - 2*socanbu
+                        elif sub_daliyEnergy.count(max(sub_daliyEnergy)) == 4:
+                            add = max(sub_daliyEnergy) + (socanbu - (socanbu//5000)*5000)
+                            phandu = 4*(socanbu//5000)*5000 - 3*socanbu
+                        elif sub_daliyEnergy.count(max(sub_daliyEnergy)) == 5:
+                            add = max(sub_daliyEnergy) + (socanbu - (socanbu//5000)*5000)
+                        else:
+                            print(f"Out range for max peak!. Max peak is: {sub_daliyEnergy.count(max(sub_daliyEnergy))}")
+                        energy_15p.append(add)
 
-if max(round_dailyEnergy) <= web_dailyEnergy:
-    socanbu = web_dailyEnergy - max(round_dailyEnergy)
-    for add in sub_daliyEnergy:
-        if socanbu <= 5000:
-            if add == max(sub_daliyEnergy):
-                if sub_daliyEnergy.count(max(sub_daliyEnergy)) == 1:
-                    add = max(sub_daliyEnergy) + socanbu
-                    phandu = 0
-                elif sub_daliyEnergy.count(max(sub_daliyEnergy)) == 2:
-                    if socanbu % 2000 == 0:
-                        add = max(sub_daliyEnergy) + socanbu
-                    elif socanbu % 2000 != 0:
-                        add = max(sub_daliyEnergy)  + (socanbu//2000)*1000
-                        phandu = socanbu - 2000*(socanbu//2000)
-                elif sub_daliyEnergy.count(max(sub_daliyEnergy)) == 3:
-                    if socanbu % 3000 == 0:
-                        add = max(sub_daliyEnergy) + socanbu
-                    elif socanbu % 3000 != 0:
-                        add = max(sub_daliyEnergy) + (socanbu//3000)*1000
-                        phandu = socanbu - 3000*(socanbu//3000)   
-                else:
-                    print(f"Out range!!")
-                energy_15p.append(add)
-                
-            elif add != max(sub_daliyEnergy):
-                add = add
-                energy_15p.append(add)
-                
-               
-        elif 5000 < socanbu < 15000:
-            if add == max(sub_daliyEnergy):
-                if sub_daliyEnergy.count(max(sub_daliyEnergy)) == 1:
-                    add = max(sub_daliyEnergy) + (socanbu - (socanbu//5000)*5000)
-                    phandu =(socanbu//5000)*5000
-                elif sub_daliyEnergy.count(max(sub_daliyEnergy)) == 2:
-                    ####
+                    elif add != max(sub_daliyEnergy):
+                        add = add              
+                        energy_15p.append(add)   
 
-                    pass
-                energy_15p.append(add)
+            if 0 < socanbu <= 5000:
+                count = 0
+                if phandu == 0: 
+                    for energy in energy_15p:
+                        energy = active_energy[count] + energy
+                        active_energy.append(energy)
+                        count += 1
+                elif phandu != 0:
+                    energy_15p[26] = energy_15p[26] + phandu 
+                    for energy in energy_15p:
+                        energy = active_energy[count] + energy
+                        active_energy.append(energy)
+                        count += 1
+            elif socanbu > 5000:
+                count = 0
+                m = 0
+                if phandu == 0: 
+                    for energy in energy_15p:
+                        energy = active_energy[count] + energy
+                        active_energy.append(energy)
+                        count += 1
+                elif phandu != 0:
+                    while m != (((socanbu//5000)*5000)/1000):
+                        energy_15p[20+m] = energy_15p[20+m] + 1000
+                        m += 1          
+                    for energy in energy_15p:
+                        energy = active_energy[count] + energy
+                        active_energy.append(energy)
+                        count += 1   
+            
+        elif max(round_dailyEnergy) > web_dailyEnergy:
+            socanbu = max(round_dailyEnergy) - web_dailyEnergy 
+            for add in sub_daliyEnergy:
+                if socanbu >= 0:
+                    if add == max(sub_daliyEnergy):
+                        add = max(sub_daliyEnergy)
+                        phandu = socanbu
+                        energy_15p.append(add)
+                    elif add != max(sub_daliyEnergy):
+                        add = add
+                        energy_15p.append(add)
 
-            elif add != max(sub_daliyEnergy):
-                add = add
-                energy_15p.append(add)        
-        
-    pass
-    
-elif max(round_dailyEnergy) > web_dailyEnergy:
-    socanbu = max(round_dailyEnergy) - web_dailyEnergy     
+            count = 0
+            m = 0
+            if 0 not in activePower[19:45] and phandu != 0:
+                while m != (socanbu/1000):
+                    energy_15p[20+m] = energy_15p[20+m] - 1000
+                    m += 1 
+                for energy in energy_15p:
+                    energy = active_energy[count] + energy         
+                    active_energy.append(energy)
+                    count += 1
+            elif phandu == 0:
+                energy_15p = energy_15p
+                for energy in energy_15p:
+                    energy = active_energy[count] + energy
+                    active_energy.append(energy)
+                    count += 1
+            else:
+                print("Co gia tri power bang 0.")
 
-    pass
+        delete_firstvalue = active_energy.pop(0)
 
-active_energy = [thamchieu]
-if socanbu <= 5000:
-    count = 0
-    if phandu == 0: 
-        for energy in energy_15p:
-            energy = active_energy[count] + energy
-            active_energy.append(energy)
-            count += 1
-    elif phandu != 0:
-        energy_15p[26] = energy_15p[26] + phandu 
-        for energy in energy_15p:
-            energy = active_energy[count] + energy
-            active_energy.append(energy)
-            count += 1
-elif 5000 < socanbu < 15000:
-    count = 0
-    if phandu == 0: 
-        for energy in energy_15p:
-            energy = active_energy[count] + energy
-            active_energy.append(energy)
-            count += 1
-    elif phandu != 0:
-        energy_15p[22] = energy_15p[22] + (socanbu//5000)*1000
-        energy_15p[23] = energy_15p[23] + (socanbu//5000)*1000
-        energy_15p[24] = energy_15p[24] + (socanbu//5000)*1000
-        energy_15p[25] = energy_15p[25] + (socanbu//5000)*1000
-        energy_15p[26] = energy_15p[26] + (socanbu//5000)*1000 
-        for energy in energy_15p:
-            energy = active_energy[count] + energy
-            active_energy.append(energy)
-            count += 1
-
-delete_firstvalue = active_energy.pop(0)
-
-
-print(sub_daliyEnergy)
-print(sum(sub_daliyEnergy))
-print(energy_15p)
-print(sum(energy_15p))
-# print(active_energy)
-# print(len(active_energy))
-
-
-
-
-
-
-# if max(active_energy) - min(active_energy) == web_dailyEnergy:
-#     list_Asset = [input("Nhap vao Asset: ")]
-#     list_Scope = [input("Nhap vao Scope: ")]
-#     dataframe = pd.DataFrame(
-#         {
-#             "time" : df_2["time"],
-#             "asset" : list_Asset*53,
-#             "scope" : list_Scope*53,
-#             "active_power" : df_1["active_power"],
-#             "active_energy" :  active_energy,
-#         }
-#     )
-#     dataframe.to_csv("inverter.csv", index=False)
-#     print(f"Successfully!")
-# else: 
-#     print(f"Gia tri tinh toan {max(active_energy) - min(active_energy)} khong trung khop voi gia tri {web_dailyEnergy} tren website hang!!")
+        if max(active_energy) - min(active_energy) == web_dailyEnergy:
+            dataframe = pd.DataFrame(
+                {
+                    "time" : time["time"],
+                    "asset" : asset,
+                    "scope" : scope,
+                    "active_power" : [None]*22 + activePower + [None]*19,
+                    "active_energy" : [None]*22 + active_energy + [None]*19,
+                }
+            )
+            dataframe.to_csv("inverter.csv", index=False)
+            print(f"Successfully!")
+        else: 
+            print(f"Gia tri tinh toan {max(active_energy) - min(active_energy)} khong trung khop voi gia tri {web_dailyEnergy} tren website hang!!")
---
+++ main.py
@@ -0,0 +1,8 @@
+from create_csv import CreateCSVfile
+
+CreateCSVfile.MyInput(
+    asset = "ABB-111871-3Q15-3720",
+    scope= "e5207ce3-5911-4ce3-877f-be4e245e9ddf", 
+    thamchieu=90000000, 
+    web_dailyEnergy=423000
+    )
