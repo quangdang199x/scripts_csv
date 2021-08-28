1c4cbe018365f7d3bed0d3dc774d8c5281795434
Quang <phamdangquangbk@gmail.com>
2021-08-28 21:50:45 +0700

    Add new update

--- Run.py
+++ Run.py
@@ -10,107 +10,98 @@ def yaml_loader(filepath):
 filepath = "inputs.yml"
 data = yaml_loader(filepath)
 devices = data["inverters"]
-number_day = devices[0]["number_day"]
-number_inverter = devices[1]["number_inverter"]
+option = devices[0]["options"]
+number_day = devices[1]["number_day"]
+number_inverter = devices[2]["number_inverter"]
+generated_energy = devices[3]["generated_energy"]
+list_days = devices[4]["days"]
 
 def get_entity_name():
     entity_name = []
-    count = 4
-    for value in devices[4: ]:
+    count = 5
+    for value in devices[5: ]:
         value = devices[count]["entity_name"]
         entity_name.append(value)
         count += 1
     del entity_name[number_inverter: ]
     return entity_name
-def tt_web_energy():
-    tt_web_energy = [devices[2]["generated_energy"]["day_1"], devices[2]["generated_energy"]["day_2"], devices[2]["generated_energy"]["day_3"], devices[2]["generated_energy"]["day_4"], 
-                    devices[2]["generated_energy"]["day_5"], devices[2]["generated_energy"]["day_6"], devices[2]["generated_energy"]["day_7"]]
-    return tt_web_energy
-def list_day():
-    list_day = [devices[3]["days"]["day_1"], devices[3]["days"]["day_2"], devices[3]["days"]["day_3"], 
-                devices[3]["days"]["day_4"], devices[3]["days"]["day_5"], devices[3]["days"]["day_6"], devices[3]["days"]["day_7"]]
-    return list_day
+
 def list_asset():
     list_asset_1 = []
-    count = 4
-    for value in devices[4: ]:
+    count = 5
+    for value in devices[5: ]:
         value = devices[count]["asset"]
         list_asset_1.append(value)
         count += 1
     return list_asset_1
 def list_scope():
     list_scope_1 = []
-    count = 4
-    for value in devices[4: ]:
+    count = 5
+    for value in devices[5: ]:
         value = devices[count]["scope"]
         list_scope_1.append(value)
         count += 1
     return list_scope_1
 def list_active_energy():
     list_active_energy = []
-    count = 4
-    for value in devices[4: ]:
+    count = 5
+    for value in devices[5: ]:
         value = devices[count]["active_energy"]
         list_active_energy.append(value)
         count += 1
     return list_active_energy
-###Sheet_data (no change!):
-class Output_day:
-    day_1_Setup = Timeline(day_month_year=list_day()[0]).get_time()
-    day_2_Setup = Timeline(day_month_year=list_day()[1]).get_time()
-    day_3_Setup = Timeline(day_month_year=list_day()[2]).get_time()
-    day_4_Setup = Timeline(day_month_year=list_day()[3]).get_time()
-    day_5_Setup = Timeline(day_month_year=list_day()[4]).get_time()
-    day_6_Setup = Timeline(day_month_year=list_day()[5]).get_time()
-    day_7_Setup = Timeline(day_month_year=list_day()[6]).get_time()
+
+def output_days():
+    list_od = []
+    for day in list_days:
+        day_set = Timeline(day).get_time()
+        list_od.append(day_set)
+    return list_od
+
 ##Inputs:
-daily_energy = entity_Name(colums_Name=get_entity_name()).entity_name()
+web_inverter = entity_Name(colums_Name=get_entity_name()).entity_name()
+co_web = entity_Name(options=option, colums_Name=get_entity_name()).en_entity_name()
 columns_name = get_entity_name()
-total_dailyEnergy = tt_web_energy()
 list_sheet_days = [sheet_day("Day_1"), sheet_day("Day_2"), sheet_day("Day_3"), sheet_day("Day_4"), sheet_day("Day_5"), sheet_day("Day_6"), sheet_day("Day_7")]
-list_output_days = [Output_day.day_1_Setup, Output_day.day_2_Setup, Output_day.day_3_Setup, Output_day.day_4_Setup, Output_day.day_5_Setup, Output_day.day_6_Setup, Output_day.day_7_Setup]
+list_output_days = output_days()
 list_asset_1 = list_asset()
 list_scope_1 = list_scope()
-def web_inverter():
-    web_inverter = []
-    for en in daily_energy:
-        en = en
-        web_inverter.append(en)
-    return web_inverter    
+
 def cov_Wh():
     count = 0
     list_1 = []
     for value in list_active_energy()[0: number_inverter]:
         value = value
-        days_Wh = get_last_day_list(list_active_energy()[count], web_inverter()[count]).get_list()
+        days_Wh = get_last_day_list(option, list_active_energy()[count], co_web[count]).get_list()
         list_1.append(days_Wh)
         count += 1
     return list_1
+
 def final_df():
     x = 0
     y = 1
     fn_df = []
     while x != number_inverter:
-        inverter = Inverter_for_days(number_Days = number_day, list_sheet_days = list_sheet_days, columns_name=columns_name[x], days_enery=cov_Wh()[x], list_web_energy_inverter = web_inverter()[x], list_output_days=list_output_days, asset_name=list_asset_1[x], scope_name=list_scope_1[x])
+        inverter = Inverter_for_days(options=option , number_Days = number_day, list_sheet_days = list_sheet_days, columns_name=columns_name[x], days_enery=cov_Wh()[x], list_web_energy_inverter = web_inverter[x], list_output_days=list_output_days, asset_name=list_asset_1[x], scope_name=list_scope_1[x])
         fn_df.append(inverter.create_df())
         x += 1
     while y != len(fn_df):
         fn_df[0] = fn_df[0].append(fn_df[y])
         y += 1
     return fn_df[0]
-    
+
 if __name__ == "__main__":
-    check_data = Check_final_data(number_inverter=number_inverter, list=web_inverter()).result()
+    check_data = Check_final_data(number_inverter=number_inverter, list=web_inverter).result()
     if number_inverter == 1:
-        if check_data == tt_web_energy():
+        if check_data == generated_energy:
             single_df(final_df()[0])
-            print("Successfully for creating CSV files!")
+            print(f"Successfully for creating CSV files! {len(final_df())} rows have created.")
         else:
-            print(f"Error! List total website energy: {tt_web_energy()} doesn't equal list check_data: {check_data}!")
+            print(f"Error! List total website energy: {generated_energy} doesn't equal list check_data: {check_data}!")
     elif number_inverter != 1:
-        if check_data == tt_web_energy():
+        if check_data == generated_energy:
             single_df(final_df())
-            print("Successfully for creating CSV files!")
+            print(f"Successfully for creating CSV files! {len(final_df())} rows have created.")
         else:
-            print(f"Error! List total website energy: {tt_web_energy()} doesn't equal list check_data: {check_data}!")
+            print(f"Error! List total website energy: {generated_energy} doesn't equal list check_data: {check_data}!")
             
--- create_csv.py
+++ create_csv.py
@@ -7,7 +7,8 @@ def sheet_day(sheet_data = None):
     return read_file
 
 class Inverter:
-    def __init__(self, dataframe = None, latest_day_energy = None, web_dailyEnergy = None):
+    def __init__(self, options = None, dataframe = None, latest_day_energy = None, web_dailyEnergy = None):
+        self.options = options
         self.dataframe = dataframe
         self.latest_day_energy = latest_day_energy
         self.web_dailyEnergy = web_dailyEnergy
@@ -50,7 +51,8 @@ class Inverter:
             x += 1
         return subtract_dailyEnergy  
 
-    def increase_activeEnergy(self):
+    def activeEnergy(self):
+        op = self.options
         energy_15min = []
         active_energy = [self.latest_day_energy]
         count_1 = 0
@@ -58,44 +60,76 @@ class Inverter:
         for value in self.subtract_dailyEnergy():
             value = value
             energy_15min.append(value)
-        if max(self.round_dailyEnergy()) <= self.web_dailyEnergy:
-            comp = self.web_dailyEnergy - max(self.round_dailyEnergy())
-            if comp <= 20:
-                while count_1 != (comp/1000):
-                    energy_15min[20+count_1] = energy_15min[20+count_1] + 1000
-                    count_1 += 1
-            elif comp > 20:
-                while count_1 != (comp/1000):
-                    energy_15min[12+count_1] = energy_15min[12+count_1] + 1000
-                    count_1 += 1
-        elif max(self.round_dailyEnergy()) > self.web_dailyEnergy:
-            comp = max(self.round_dailyEnergy()) - self.web_dailyEnergy
-            if comp <= 20:    
-                while count_1 != (comp/1000):
-                    energy_15min[20+count_1] = energy_15min[20+count_1] - 1000
-                    count_1 += 1
-            elif comp > 20:
-                while count_1 != (comp/1000):
-                    energy_15min[15+count_1] = energy_15min[15+count_1] - 1000
-                    count_1 += 1
-        for energy in energy_15min:
-            energy = active_energy[count_2] + energy
-            active_energy.append(energy)
-            count_2 += 1
-        active_energy.pop(0)
-        if max(active_energy) - min(active_energy) == self.web_dailyEnergy:
-            return active_energy
+        if op == "increase":
+            if max(self.round_dailyEnergy()) <= self.web_dailyEnergy:
+                comp = self.web_dailyEnergy - max(self.round_dailyEnergy())
+                if comp <= 20:
+                    while count_1 != (comp/1000):
+                        energy_15min[20+count_1] = energy_15min[20+count_1] + 1000
+                        count_1 += 1
+                elif comp > 20:
+                    while count_1 != (comp/1000):
+                        energy_15min[12+count_1] = energy_15min[12+count_1] + 1000
+                        count_1 += 1
+            elif max(self.round_dailyEnergy()) > self.web_dailyEnergy:
+                comp = max(self.round_dailyEnergy()) - self.web_dailyEnergy
+                if comp <= 20:    
+                    while count_1 != (comp/1000):
+                        energy_15min[20+count_1] = energy_15min[20+count_1] - 1000
+                        count_1 += 1
+                elif comp > 20:
+                    while count_1 != (comp/1000):
+                        energy_15min[15+count_1] = energy_15min[15+count_1] - 1000
+                        count_1 += 1
+
+            for energy in energy_15min:
+                energy = active_energy[count_2] + energy
+                active_energy.append(energy)
+                count_2 += 1
+            active_energy.pop(0)
+            if max(active_energy) - min(active_energy) == self.web_dailyEnergy:
+                return active_energy
+            else:
+                sys.exit()
+        elif op == "decrease":
+            if max(self.round_dailyEnergy()) <= self.web_dailyEnergy:
+                comp = self.web_dailyEnergy - max(self.round_dailyEnergy())
+                if comp <= 20:
+                    while count_1 != (comp/1000):
+                        energy_15min[20+count_1] = energy_15min[20+count_1] + 1000
+                        count_1 += 1
+                    energy_15min.reverse()
+                elif comp > 20:
+                    while count_1 != (comp/1000):
+                        energy_15min[12+count_1] = energy_15min[12+count_1] + 1000
+                        count_1 += 1
+                    energy_15min.reverse()
+            elif max(self.round_dailyEnergy()) > self.web_dailyEnergy:
+                comp = max(self.round_dailyEnergy()) - self.web_dailyEnergy
+                if comp <= 20:    
+                    while count_1 != (comp/1000):
+                        energy_15min[20+count_1] = energy_15min[20+count_1] - 1000
+                        count_1 += 1
+                    energy_15min.reverse()
+                elif comp > 20:
+                    while count_1 != (comp/1000):
+                        energy_15min[15+count_1] = energy_15min[15+count_1] - 1000
+                        count_1 += 1
+                    energy_15min.reverse()
+            
+            for energy in energy_15min:
+                energy = active_energy[count_2] - energy
+                active_energy.append(energy)
+                count_2 += 1
+            active_energy.pop(0)
+            active_energy.reverse()
+            if max(active_energy) - min(active_energy) == self.web_dailyEnergy:
+                return active_energy
+            else:
+                sys.exit()
         else:
             sys.exit()
 
-    def check_value(self):
-        check = max(self.increase_activeEnergy()) - min(self.increase_activeEnergy())
-        return check   
-    
-    def latest_Energy(self):
-        max_energy = max(self.increase_activeEnergy())
-        return max_energy
-        
     def create_CSV_files(self, asset = None, scope = None, time = None):
         dataframe = pd.DataFrame(
             {
@@ -103,7 +137,7 @@ class Inverter:
                 "asset" : asset,
                 "scope" : scope,
                 "active_power" : [None]*22 + self.get_power_inverter() + [None]*19,
-                "active_energy" : [None]*22 + self.increase_activeEnergy() + [None]*19,
+                "active_energy" : [None]*22 + self.activeEnergy() + [None]*19,
             }
         )
         return dataframe      
@@ -116,7 +150,8 @@ class Timeline:
         return get_time[self.day_month_year]
 
 class Setup_Inverter:
-    def __init__(self, choose_sheet_day=None , choose_columns=None, latest_day_energy=None, web_dailyEnergy=None, output_day=None, asset=None, scope=None):
+    def __init__(self, options = None, choose_sheet_day=None , choose_columns=None, latest_day_energy=None, web_dailyEnergy=None, output_day=None, asset=None, scope=None):
+        self.options = options
         self.choose_sheet_day = choose_sheet_day
         self.output_day = output_day
         self.choose_columns = choose_columns
@@ -126,28 +161,40 @@ class Setup_Inverter:
         self.scope = scope
     
     def setup_1(self):
-        inverter = Inverter(dataframe=DataFrame(self.choose_sheet_day, columns=[self.choose_columns]).values, latest_day_energy=self.latest_day_energy ,web_dailyEnergy=self.web_dailyEnergy)
+        inverter = Inverter(options= self.options, dataframe=DataFrame(self.choose_sheet_day, columns=[self.choose_columns]).values, latest_day_energy=self.latest_day_energy ,web_dailyEnergy=self.web_dailyEnergy)
         return inverter
     def get_dataframe(self):
         df = self.setup_1().create_CSV_files(time=self.output_day, asset=self.asset, scope=self.scope)
         return df
 
 class get_last_day_list:
-    def __init__(self, last_day_energy=None, website_energy_inverter=None):
+    def __init__(self, options=None, last_day_energy=None, website_energy_inverter=None):
+        self.options=options
         self.last_day_energy = last_day_energy
         self.website_energy_inverter = website_energy_inverter
     def get_list(self):
+        opt = self.options
         list_last_day_energy = [self.last_day_energy]
         count_1 = 0
-        for value in self.website_energy_inverter:
-            value = value + list_last_day_energy[count_1]
-            list_last_day_energy.append(value)
-            count_1 += 1
-        list_last_day_energy.pop(-1)
-        return list_last_day_energy
+        if opt == "increase":
+            for value in self.website_energy_inverter:
+                value = value + list_last_day_energy[count_1]
+                list_last_day_energy.append(value)
+                count_1 += 1
+            list_last_day_energy.pop(-1)
+            return list_last_day_energy
+        elif opt == "decrease":
+            for value in self.website_energy_inverter:
+                value = list_last_day_energy[count_1] - value
+                list_last_day_energy.append(value)
+                count_1 += 1
+            list_last_day_energy.pop(-1)
+            list_last_day_energy.reverse()
+            return list_last_day_energy
 
 class Inverter_for_days:
-    def __init__(self, number_Days = None, list_sheet_days=None, columns_name=None, days_enery=None, list_web_energy_inverter=None, list_output_days=None, asset_name=None, scope_name=None):
+    def __init__(self, options = None, number_Days = None, list_sheet_days=None, columns_name=None, days_enery=None, list_web_energy_inverter=None, list_output_days=None, asset_name=None, scope_name=None):
+        self.options = options
         self.number_Days = number_Days
         self.list_sheet_days = list_sheet_days
         self.columns_name = columns_name
@@ -162,6 +209,7 @@ class Inverter_for_days:
         y = 1
         while x != self.number_Days:
             per_inverter = Setup_Inverter(
+                options=self.options,
                 choose_sheet_day=self.list_sheet_days[x],
                 choose_columns=self.columns_name,
                 latest_day_energy=self.days_energy[x],
@@ -179,7 +227,8 @@ class Inverter_for_days:
         return df_tem[0]
 
 class entity_Name:
-    def __init__(self, colums_Name = None):
+    def __init__(self, options = None, colums_Name = None):
+        self.options = options
         self.columns_Name = colums_Name
     def entity_name(self):
         n_list = []
@@ -190,8 +239,23 @@ class entity_Name:
             for value in values:
                 value = int(value)*1000
                 list_1.append(value)
-            list_1.extend([0]*(7 - len(list_1)))
             n_list.append(list_1)
+            counts += 1
+        return n_list
+    def en_entity_name(self):
+        n_list = []
+        counts = 0
+        while counts != len(self.columns_Name):
+            values = DataFrame(sheet_day("Daily_energy"), columns=[self.columns_Name[counts]]).values    
+            list_1 = []        
+            for value in values:
+                value = int(value)*1000
+                list_1.append(value)
+            if self.options == "increase":
+                n_list.append(list_1)
+            elif self.options == "decrease":
+                list_1.reverse()
+                n_list.append(list_1)
             counts += 1
         return n_list
 
@@ -210,6 +274,6 @@ class Check_final_data:
             count += 1
         resuft = self.list.pop(-1)
         return resuft
-        
+
 def single_df(df=None):
     return df.to_csv("inverter.csv", index=False)
